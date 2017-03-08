ba1c49a2682864fcb7b8bc212bfed38dd9767001
SLPowerCoder <sunlei_1030@126.com>
2017-03-08 12:51:48 +0800

    Initial Commit

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by 孙磊 on 2017/3/8.
+//  Copyright © 2017年 孙磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
